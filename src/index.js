4994adfe96e68917a162f6b75a7d15b4de098f4a
iravench <iravench@users.noreply.github.com>
2016-05-02 12:18:37 +1000

    move fm_selector into ticket controller

--- src/index.js
+++ src/index.js
@@ -6,26 +6,14 @@ import express from 'express'
 import controllers from './controllers'
 import bodyParser from 'body-parser'
 
-import repo_impl from './implementations/repo_impl';
-import repo_factory from './repo_factory';
-import fm_token_factory from './fm_token_factory';
-import fm_token_impl from './implementations/fm_token_impl';
-import fm_policy_factory from './fm_policy_factory';
-import fm_selector_factory from './fm_selector_factory';
-
-const repo = repo_factory({ impl: repo_impl });
-const fm_policy = fm_policy_factory({ repo: repo });
-const fm_token = fm_token_factory({ impl: fm_token_impl });
-const fm_selector = fm_selector_factory({ repo: repo, policy: fm_policy, token: fm_token });
 const log = logger.child({module: 'index'});
-
 const app = express();
-app.locals.fm_selector = fm_selector;
 const apiRouter = express.Router();
 controllers.init(apiRouter);
-app.use('/v1', bodyParser.json(), bodyParser.urlencoded({ extended:true }), apiRouter);
 
-//a sample html page demonstrating how a client can obtain a ws connection
+//bind api routes
+app.use('/v1', bodyParser.json(), bodyParser.urlencoded({ extended:true }), apiRouter);
+//bind a sample html page demonstrating how a client can obtain a ws connection
 app.use(express.static(__dirname + '/public'));
 
 app.listen(config.port);
