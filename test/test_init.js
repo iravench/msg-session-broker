7ebd1d6effe98e4df8cfc947ffb35084853eb82c
iravench <iravench@users.noreply.github.com>
2016-05-03 17:21:24 +1000

    switch from socket.io to primus

--- test/test_init.js
+++ test/test_init.js
@@ -1,9 +1,7 @@
 'use strict';
 
 import chai from 'chai';
-import chaiImmutable from 'chai-immutable';
 import sinonChai from 'sinon-chai';
 
-chai.use(chaiImmutable);
 chai.use(sinonChai);
 process.env.NODE_ENV = 'test';
